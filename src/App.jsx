b784a74597804669fd17c0a8ec63cd6ae12a5d62
xXXkkmXx <maxkowalczyk1111@gmail.com>
2026-07-12 23:22:20 +0200

    added simply styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import "bootstrap/dist/css/bootstrap.css"
+
 function App (){
   return(
-    <div>
-      <h2>ReWatcher</h2>
+    <div id='header' className="border border-dark">
+      <li>
+        <ul><a href="#"><h2>Rewatcher</h2></a></ul>
+        <ul><a href="#">Main</a></ul>
+        <ul><a href="#">Calendar</a></ul>
+        <ul><a href="#">Log In</a></ul>
+      </li>
     </div>
   )
 }

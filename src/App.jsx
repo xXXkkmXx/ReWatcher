0a65d977dab6cea69b04167dd8da4a6ef6483524
xXXkkmXx <maxkowalczyk1111@gmail.com>
2026-07-16 22:17:46 +0200

    added system of the changing theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
+import { useEffect } from "react";
+
 import "bootstrap/dist/css/bootstrap.css"
 
+const Toggle = () =>{
+  const newTheme = localStorage.getItem("theme") == "dark" ? "light" : "dark";
+  document.querySelector("html").setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme);  
+  document.getElementById("toggleBtn").src = newTheme === "dark" ? "/moon.svg" : "/sun.svg";
+}
+
 function App (){
+  const DetectingTheme = () =>{
+    return window.matchMedia("(prefers-color-scheme: dark)") ? "dark" : "light";
+  }
+  const LoadTheme = () =>{
+    document.querySelector("html").setAttribute("data-theme",DetectingTheme());
+    localStorage.setItem("theme", DetectingTheme());
+    document.getElementById("toggleBtn").src = DetectingTheme() === "dark" ? "/moon.svg" : "/sun.svg";
+  }
+
+  useEffect(()=>{
+    LoadTheme();
+  },[]); 
+
   return(
     <div id='header' className="border border-dark">
       <li>
@@ -9,6 +31,7 @@ function App (){
         <ul><a href="#">Calendar</a></ul>
         <ul><a href="#">Log In</a></ul>
       </li>
+      <img id="toggleBtn" src="/sun.svg" onClick={Toggle}></img>
     </div>
   )
 }

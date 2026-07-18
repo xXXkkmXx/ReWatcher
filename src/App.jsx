662bf50a09f7ce727909fe90136e60936d2f2bf4
xXXkkmXx <maxkowalczyk1111@gmail.com>
2026-07-18 19:50:35 +0200

    Adding a basic system of routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import { useEffect } from "react";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import ReactDOM from "react-dom";
+
+import Calendar from "./Calendar";
+import LoginScreen from "./LoginScreen";
 
 import "bootstrap/dist/css/bootstrap.css"
+
+const Error404 = () =>{
+  return (
+    <>
+      <h2>Page was not found :(</h2>
+    </>
+  )
+}
 
 const Toggle = () =>{
   const newTheme = localStorage.getItem("theme") == "dark" ? "light" : "dark";
@@ -24,15 +37,27 @@ function App (){
   },[]); 
 
   return(
-    <div id='header' className="border border-dark">
-      <li>
-        <ul><a href="#"><h2>Rewatcher</h2></a></ul>
-        <ul><a href="#">Main</a></ul>
-        <ul><a href="#">Calendar</a></ul>
-        <ul><a href="#">Log In</a></ul>
-      </li>
-      <img id="toggleBtn" src="/sun.svg" onClick={Toggle}></img>
-    </div>
+    <>
+      <BrowserRouter>
+        <div id='header' className="border border-dark">
+          <li>
+            <ul><h2>Rewatcher</h2></ul>
+            <ul><a href="/">Main</a></ul>
+            <ul><a href="/Calendar">Calendar</a></ul>
+            <ul><a href="/Login">Log In</a></ul>
+          </li>
+          <img id="toggleBtn" src="/sun.svg" onClick={Toggle}></img>
+        </div>
+        <div id="Content">
+          <Routes>
+            <Route exact path="/" Component={()=>{<h1>Home</h1>}}/>
+            <Route path="/Calendar" Component={Calendar}/>
+            <Route path="/Login" Component={LoginScreen}/>
+            <Route path="*" Component={Error404}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ src/LoginScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const LoginScreen = () =>{
+    return(
+        <div>
+            <h2>Login Screen</h2>
+        </div>
+    )
+}
+
+export default LoginScreen;

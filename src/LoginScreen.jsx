f14b08e5e9346c2a4f44ba6a09e9dada2ebf241d
xXXkkmXx <maxkowalczyk1111@gmail.com>
2026-07-18 22:36:27 +0200

    added view of the login screen

--- src/LoginScreen.jsx
+++ src/LoginScreen.jsx
@@ -1,9 +1,27 @@
+import { Button } from "bootstrap";
 import React from "react"
 
 const LoginScreen = () =>{
     return(
-        <div>
-            <h2>Login Screen</h2>
+        <div id="LoginTab" className="border border-dark">
+            <h2 className="border-bottom border-dark">Login</h2>
+            <p>
+                <label>username:</label>
+            </p>
+            <p> 
+                <input className="border-bottom border-secondary" type="text"/>
+            </p>
+            <p>
+                <label>password:</label>
+            </p>
+            <p>
+                <input type="text"/>
+            </p>
+            <a href="#">you forgot a password?</a>
+            <p>
+                <button className="border border-dark">Login</button>
+                <button className="border border-dark">Sign in</button>
+            </p>
         </div>
     )
 }
